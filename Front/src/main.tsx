7c81a797925cf1c605e6e7ac3f58165363e02582
ParkSeYun98 <yun12343@naver.com>
2023-11-29 17:49:43 +0900

    TailwindCSS 적용

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import { RecoilRoot } from 'recoil';
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <RecoilRoot>
     <App />
-  </React.StrictMode>,
+  </RecoilRoot>
 )
+
---
+++ Front/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+      extend: {
+        zIndex: {
+          'negative': '-10',
+        }
+      },
+    },
+    plugins: [],
+  }
+  
+  
+  
