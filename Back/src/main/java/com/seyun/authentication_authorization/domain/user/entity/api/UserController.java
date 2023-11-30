c8c80cad4666e4a51bd7045a9e745d8c3f883e82
ParkSeYun98 <yun12343@naver.com>
2023-11-30 13:37:25 +0900

    test

--- Back/src/main/java/com/seyun/authentication_authorization/domain/user/entity/api/UserController.java
+++ Back/src/main/java/com/seyun/authentication_authorization/domain/user/entity/api/UserController.java
@@ -2,12 +2,13 @@ package com.seyun.authentication_authorization.domain.user.entity.api;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Getter
+@Getter @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
